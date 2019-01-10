19d14856e7494f33063b66d2de91e30294bf12c4
Git <git@github.com>
2019-01-10 14:10:29 +0100

    Update checkCashRegister(price, cash, cid)

--- index.js
+++ index.js
@@ -1,18 +1,45 @@
 function checkCashRegister(price, cash, cid) {
-    var change;
-    // Here is your change, ma'am.
-    return change;
+  let cidCopy = []
+  for(let i = 0; i < cid.length; i++) {
+    cidCopy.push([])
+      cidCopy[i].push(cid[i][0])
+      cidCopy[i].push(0)
   }
-  
-  // Example cash-in-drawer array:
-  // [["PENNY", 1.01],
-  // ["NICKEL", 2.05],
-  // ["DIME", 3.1],
-  // ["QUARTER", 4.25],
-  // ["ONE", 90],
-  // ["FIVE", 55],
-  // ["TEN", 20],
-  // ["TWENTY", 60],
-  // ["ONE HUNDRED", 100]]
-  
-  checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
+  //
+  let changeDue = cash - price
+  if(changeDue >= 0.25 && cid[3][1] != 0) {
+    changeDue -= 0.25
+    cid[3][1] -= 0.25
+    cidCopy[3][1] += 0.25
+  }
+  if(changeDue >= 0.25 && cid[3][1] != 0) {
+    changeDue -= 0.25
+    cid[3][1] -= 0.25
+    cidCopy[3][1] += 0.25
+  }
+  //
+  let change = []
+  for(let i = 0; i < cidCopy.length; i++) {
+    if(cidCopy[i][1] != 0) {
+      change.push(cidCopy[i])
+    }
+  }
+  var returnValue = {
+    status: "OPEN",
+    change: change
+  }
+  return returnValue;
+}
+
+// Example cash-in-drawer array:
+// [["PENNY", 1.01],
+// ["NICKEL", 2.05],
+// ["DIME", 3.1],
+// ["QUARTER", 4.25],
+// ["ONE", 90],
+// ["FIVE", 55],
+// ["TEN", 20],
+// ["TWENTY", 60],
+// ["ONE HUNDRED", 100]]
+
+console.log(checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]])[3])
